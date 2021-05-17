2991a6c59d3ea188017ea596d7279c1576236e17
unknown <mihai.badi@yahoo.com>
2021-05-17 10:12:49 +0300

    skybox_v1.0

---
+++ build/precache-manifest.ce83dbe2d7dd62f91dd81a6369508cfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21cede5bb3822717f44d9ec693e5206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "894f8d58fadaf00f7b18",
+    "url": "/static/css/main.ef83dd4b.chunk.css"
+  },
+  {
+    "revision": "6a9e805aee95386b4bde",
+    "url": "/static/js/2.e2e8a705.chunk.js"
+  },
+  {
+    "revision": "894f8d58fadaf00f7b18",
+    "url": "/static/js/main.494b9351.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
